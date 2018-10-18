94d46cb9a9e153be76ff01ef25c520b4b07b571b
Mariana <marangoni.mariana@gmail.com>
2018-10-17 22:33:29 -0300

    implementando word count e text color

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
-
+$(document).ready(function(){
 
 $("#btn-submit").click(function(e) {
     e.preventDefault();
-    console.log($("#xuxu").val("xis"));
+    $("#xuxu").val();
     insereTweet();
-
+    $("#xuxu").val("");
 });
 
-function insereTweet(){    
-    let divInserir = document.querySelector("tuites");
-    divInserir.innerHTML = `
-    <p id="pezinho">${$("#xuxu").val("xis")}</p>
-    `
-}
 
+
+let data = new Date();
+const meses = ["janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+  "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+];
+
+
+function insereTweet(){    
+    $('.tuites').prepend(`
+    <div class="jumbotron col-6 mt-4 mx-auto word-wrap break-word ">
+    <p class="h2">${$("#xuxu").val()}</p>
+    <p class="text-dark" id="datepicker">${data.getDate()} de ${meses[data.getMonth()]}, ${data.getFullYear()}</p>
+    </div>
+    `)
+};
+
+
+function pegaNumeroCaracteres(){
+        let charSobrando = 280 - $("#xuxu").val().length;
+
+        $("#xuxu").removeClass('text-warning');
+        $("#xuxu").removeClass('text-success');
+        $("#xuxu").removeClass('text-danger');
+        
+        if(charSobrando <=0){
+            $("#xuxu").addClass('text-danger');
+        }
+        else if(charSobrando <= 50){
+            $("#xuxu").addClass('text-warning');
+        }
+        else if (charSobrando <= 200){
+            $("#xuxu").addClass('text-success');
+        }
+        
+        return $("#contador").text(`você ainda tem ${charSobrando} caracteres`);
+    }
+$("#xuxu").keyup(function(){
+     pegaNumeroCaracteres()
+    });
+});
 //btnSubmit.addEventListener("click", function (e) {
 //    e.preventDefault()
 //});
